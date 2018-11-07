a31451c6cbf6f5c19912efdf92f7c87e1e69e129
Luke Childs <lukechilds123@gmail.com>
2018-11-07 19:35:42 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# build-output-script
+
+> Builds a P2PKH Bitcoin transaction output script
+
+[![Build Status](https://travis-ci.org/lukechilds/build-output-script.svg?branch=master)](https://travis-ci.org/lukechilds/build-output-script)
+[![Coverage Status](https://coveralls.io/repos/github/lukechilds/build-output-script/badge.svg?branch=master)](https://coveralls.io/github/lukechilds/build-output-script?branch=master)
+[![npm](https://img.shields.io/npm/dm/build-output-script.svg)](https://www.npmjs.com/package/build-output-script)
+[![npm](https://img.shields.io/npm/v/build-output-script.svg)](https://www.npmjs.com/package/build-output-script)
+
+Builds a P2PKH Bitcoin transaction output script from an array of P2PKH addresses and amounts. Will also work with any Bitcoin derived cryptocurrencies with a single byte pubkey hash prefix.
+
+## Install
+
+```shell
+npm install build-output-script
+```
+
+## License
+
+MIT © Luke Childs
---
+++ src/index.js
@@ -0,0 +1,50 @@
+const bs58check = require('bs58check');
+const { encode: numberToCompactSizeUInt } = require('varuint-bitcoin');
+const { OP_DUP, OP_HASH160, OP_EQUALVERIFY, OP_CHECKSIG } = require('bitcoin-ops');
+
+const addressDecode = address => bs58check.decode(address).slice(1);
+
+const checkUInt53 = number => {
+	if (number < 0 || !Number.isSafeInteger(number)) {
+		throw new RangeError('value out of range');
+	}
+};
+
+const numberToUInt64 = number => {
+	checkUInt53(number);
+
+	const buffer = Buffer.alloc(8);
+
+	buffer.writeUInt32LE(number >>> 0);
+	buffer.writeUInt32LE((number / 0x100000000) | 0, 4);
+
+	return buffer;
+};
+
+const buildOutputScript = outputs => {
+	let outputScript = [...numberToCompactSizeUInt(outputs.length)];
+
+	for (const [address, value] of outputs) {
+		const pubkeyHash = addressDecode(address);
+
+		const scriptPubKey = [
+			OP_DUP,
+			OP_HASH160,
+			pubkeyHash.length,
+			...pubkeyHash,
+			OP_EQUALVERIFY,
+			OP_CHECKSIG
+		];
+
+		outputScript = [
+			...outputScript,
+			...numberToUInt64(value),
+			...numberToCompactSizeUInt(scriptPubKey.length),
+			...scriptPubKey
+		];
+	}
+
+	return Buffer.from(outputScript).toString('hex');
+};
+
+module.exports = buildOutputScript;
