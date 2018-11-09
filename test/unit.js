661059e7c082787477a74fa27ffc1fafbf5339a9
Luke Childs <lukechilds123@gmail.com>
2018-11-09 15:01:47 +0700

    Test single address and value input returns valid output script

--- test/unit.js
+++ test/unit.js
@@ -1,2 +1,9 @@
 import test from 'ava';
 import buildOutputScript from '..';
+
+test('Single address and value input returns valid output script', t => {
+	const outputScipt = buildOutputScript([['1LukeQU5jwebXbMLDVydeH4vFSobRV9rkj', 100000000]]);
+	const expectedOutputScript = '0100e1f505000000001976a914da6473ed373e08f46dd8003fca7ba72fbe9c555e88ac';
+
+	t.is(outputScipt, expectedOutputScript);
+});
