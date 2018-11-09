6608b54f6e27e213fc2848172f0e9bff59d67d35
Luke Childs <lukechilds123@gmail.com>
2018-11-09 17:25:11 +0700

    Fix formatting

--- test/unit.js
+++ test/unit.js
@@ -12,8 +12,7 @@ test('Multiple address and value inputs returns valid output script', t => {
 	const outputScipt = buildOutputScript([
 		['1LukeQU5jwebXbMLDVydeH4vFSobRV9rkj', 100000000],
 		['1BitcoinEaterAddressDontSendf59kuE', 100000000]
-	]
-	);
+	]);
 	const expectedOutputScript = '0200e1f505000000001976a914da6473ed373e08f46dd8003fca7ba72fbe9c555e88ac00e1f505000000001976a914759d6677091e973b9e9d99f19c68fbf43e3f05f988ac';
 
 	t.is(outputScipt, expectedOutputScript);
